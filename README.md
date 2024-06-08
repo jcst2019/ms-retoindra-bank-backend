ac590d6fd01331f1c783b8a8d995bd64056e53b1
jcst2019 <juansolar_4@hotmail.com>
2024-06-08 17:05:34 -0500

    commit final

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
+
 # ms-retoindra-bank-backend
-Reto Indra ===> ms-retoindra-bank-backend
+
+Este repositorio contiene el microservicio `ms-retoindra-bank-backend`, el cual es parte del sistema de gestión de clientes para el reto de Indra. El microservicio proporciona funcionalidades para administrar productos financieros de los clientes y sus datos asociados.
+
+## Ramas
+
+- `master`: Contiene la versión estable y desplegable del microservicio.
+- `2024-001`: Desarrollo de Requerimientos.
+
+## Instalación
+
+1. Clona el repositorio:
+
+   ```bash
+   git clone https://github.com/jcst2019/ms-retoindra-bank-backend.git
+
+## Uso
+El microservicio proporciona diferentes puntos finales (endpoints) REST para interactuar con los clientes del banco y sus productos financieros.
+
+A continuación, se muestran algunos ejemplos de las uris:
+
+- GET /api/bank/productFindByUniqueCode?uniqueCode={uniqueCode}: Encuentra un productos financiero por su código único.
+- GET /api/bank/customerFindByUniqueCode?uniqueCode={uniqueCode}: Encuentra un cliente por su código único.
+- GET /api/bank/informationFindByUniqueCode?uniqueCode={uniqueCode}: Encuentra un cliente con sus respectivos productos financiero por su código único
+
+### Desarrollado por:
+
+**Juan Carlos Solar Torres**
+
+📱 **Celular:** +51 951800369
+
+📧 **Correo:** juansolar4@gmail.com
+
--- src/test/java/com/reto/indra/ms_retoindra_bank_backend/MsRetoindraBankBackendApplicationTests.java
+++ src/test/java/com/reto/indra/ms_retoindra_bank_backend/MsRetoindraBankBackendApplicationTests.java
@@ -1,13 +1,109 @@
 package com.reto.indra.ms_retoindra_bank_backend;
 
+import com.reto.indra.ms_retoindra_bank_backend.controller.BankController;
+import com.reto.indra.ms_retoindra_bank_backend.model.Customer;
+import com.reto.indra.ms_retoindra_bank_backend.model.FinancialProductDocument;
+import com.reto.indra.ms_retoindra_bank_backend.model.InformationResponse;
+import com.reto.indra.ms_retoindra_bank_backend.service.BankService;
+import com.reto.indra.ms_retoindra_bank_backend.service.CustomerService;
+import com.reto.indra.ms_retoindra_bank_backend.service.FinancialProductService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+import static org.mockito.Mockito.when;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Arrays;
+import java.util.Date;
 
 @SpringBootTest
 class MsRetoindraBankBackendApplicationTests {
 
-	@Test
-	void contextLoads() {
+	@Mock
+	private FinancialProductService financialProductService;
+
+	@Mock
+	private CustomerService customerService;
+
+	@Mock
+	private BankService bankService;
+
+	@InjectMocks
+	private BankController bankController;
+
+	private Customer juanCarlos;
+
+	@BeforeEach
+	public void setup() {
+		MockitoAnnotations.initMocks(this);
+		juanCarlos = new Customer(
+				"1",
+				"QeCVfZKCyo+UFXLvrwmpVw==",
+				"Juan Carlos",
+				"Solar Torres",
+				"43032393",
+				39,
+				"Piura",
+				"1234567890",
+				"juan@example.com",
+				new Date()
+		);
 	}
 
+	@Test
+	public void testProductFindByUniqueCode() {
+		String uniqueCode = "QeCVfZKCyo+UFXLvrwmpVw==";
+		FinancialProductDocument checkingAccount = new FinancialProductDocument(null, uniqueCode, "USD", 3, "Checking Account", null, null, 0.0);
+		FinancialProductDocument savingsAccount = new FinancialProductDocument(null, uniqueCode, "USD", 1, "Savings Account", 1.5, null, null);
+		FinancialProductDocument creditCard = new FinancialProductDocument(null, uniqueCode, "USD", 2, "Credit Card", null, 0.0, null);
+		when(financialProductService.getFinancialProductsByUniqueCode(uniqueCode)).thenReturn(Flux.just(checkingAccount, savingsAccount, creditCard));
+
+		ResponseEntity<Flux<FinancialProductDocument>> response = bankController.productFindByUniqueCode(uniqueCode);
+
+		assertEquals(HttpStatus.OK, response.getStatusCode());
+		StepVerifier.create(response.getBody())
+				.expectNext(checkingAccount)
+				.expectNext(savingsAccount)
+				.expectNext(creditCard)
+				.verifyComplete();
+	}
+
+	@Test
+	public void testCustomerFindByUniqueCode() {
+		String uniqueCode = "QeCVfZKCyo+UFXLvrwmpVw==";
+		when(customerService.getCustomerByUniqueCode(uniqueCode)).thenReturn(Mono.just(juanCarlos));
+
+		ResponseEntity<Mono<Customer>> response = bankController.customerFindByUniqueCode(uniqueCode);
+
+		assertEquals(HttpStatus.OK, response.getStatusCode());
+		StepVerifier.create(response.getBody())
+				.expectNext(juanCarlos)
+				.verifyComplete();
+	}
+
+	@Test
+	public void testInformationFindByUniqueCode() {
+		String uniqueCode = "QeCVfZKCyo+UFXLvrwmpVw==";
+		FinancialProductDocument checkingAccount = new FinancialProductDocument(null, uniqueCode, "USD", 3, "Checking Account", null, null, 0.0);
+		FinancialProductDocument savingsAccount = new FinancialProductDocument(null, uniqueCode, "USD", 1, "Savings Account", 1.5, null, null);
+		FinancialProductDocument creditCard = new FinancialProductDocument(null, uniqueCode, "USD", 2, "Credit Card", null, 0.0, null);
+		InformationResponse informationResponse = new InformationResponse(juanCarlos, Arrays.asList(checkingAccount, savingsAccount, creditCard));
+		when(bankService.getCombinedResponse(uniqueCode)).thenReturn(Mono.just(informationResponse));
+
+		ResponseEntity<Mono<InformationResponse>> response = bankController.informationFindByUniqueCode(uniqueCode);
+
+		assertEquals(HttpStatus.OK, response.getStatusCode());
+		StepVerifier.create(response.getBody())
+				.expectNext(informationResponse)
+				.verifyComplete();
+	}
 }
